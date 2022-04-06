c6b6305e05acef407bd54538158e70e08faf433d
Brenton Strine <brenton.strine@gmail.com>
2022-04-06 14:44:45 -0400

    add ballrun with object

---
+++ ballrun with object/ballrun.js
@@ -0,0 +1,32 @@
+var ballObject = {
+  //radius
+  //position
+  //color
+  //velocity
+  //DOM element (document.querySelector("#ball"))
+}
+
+function constructBall() {
+  // When this function runs, we should:
+  // - set the following styles, according to ballObject
+  //   - radius
+  //   - position (starting)
+  //   - color
+}
+
+function move() {
+  setPosition();
+  drawPosition();
+}
+
+function setPosition() {
+  // when this function runs, we need to:
+  //   - update the properties of the ballObject
+  //   - run the drawPosition() function
+}
+
+function drawPosition() {
+  // when this function runs, we need to:
+  //   - use the properties of the ballObject to
+  //     update the style of the ball DOM element
+}
