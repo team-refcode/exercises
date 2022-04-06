ceabcf7b2d8580066b151143d9b3aae4c259f71b
Brenton Strine <brenton.strine@gmail.com>
2022-04-06 15:00:34 -0400

    complete ballrun and add solution

--- ballrun with object/ballrun.js
+++ ballrun with object/ballrun.js
@@ -1,17 +1,20 @@
-var ballObject = {
-  //radius
-  //position
-  //color
-  //velocity
-  //DOM element (document.querySelector("#ball"))
-}
+var ballObject = {}
 
 function constructBall() {
   // When this function runs, we should:
-  // - set the following styles, according to ballObject
-  //   - radius
-  //   - position (starting)
-  //   - color
+  // - add defaults to the ballObject
+  //      radius
+  //      position ({x: 250, y: 250})
+  //      color
+  //      velocity ({x: 10, y: 0})
+  //      domElement (document.querySelector("#ball"))
+
+  // Add the following starting styles to the domElement:
+  //  - width = radius + "px"
+  //  - height = radius + "px"
+  //  - backgroundColor = color
+  //  - top = position.y + "px"
+  //  - left = position.x + "px"
 }
 
 function move() {
@@ -20,13 +23,14 @@ function move() {
 }
 
 function setPosition() {
-  // when this function runs, we need to:
+  // When this function runs, we need to:
   //   - update the properties of the ballObject
   //   - run the drawPosition() function
+  // The position of the ball should be the current position plus the velocity.
 }
 
 function drawPosition() {
-  // when this function runs, we need to:
-  //   - use the properties of the ballObject to
-  //     update the style of the ball DOM element
+  // When this function runs, we need to:
+  //   - use the properties of the ballObject to update the style of the ball domElement
+  // Remember that CSS requires a string with "px" at the end. You can use x + "px"
 }
